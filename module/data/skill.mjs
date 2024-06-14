89036e7d11ad58b19b761aff86a3e4a119747827
GBQT <nheutte@gmail.com>
2024-06-14 21:05:34 +0200

    Added skill sheet

--- module/data/skill.mjs
+++ module/data/skill.mjs
@@ -7,9 +7,9 @@ export default class qinSkill extends qinItemBase {
         const fields = foundry.data.fields;
         const requiredInteger = { required: true, nullable: false, integer: true };
         const schema = super.defineSchema();
-
-        schema.rollModifier = new fields.StringField({ initial: "@aspects.dex.rollableModifier + @aspects.dis.rollableModifier" });
-        schema.nativeDifficulty = new fields.NumberField({ ...requiredInteger, initial: 0 });
+        
+        schema.value = new fields.NumberField({ ...requiredInteger, initial: 1 });
+        schema.relatedAspect = new fields.StringField({ initial: "wood" });
 
         return schema;
     }
--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -52,11 +52,6 @@ export class qinActorSheet extends ActorSheet {
       this._prepareCharacterData(context);
     }
 
-    // Prepare NPC data and items.
-    if (actorData.type == 'npc') {
-      this._prepareItems(context);
-    }
-
     // Add roll data for TinyMCE editors.
     context.rollData = context.actor.getRollData();
 
