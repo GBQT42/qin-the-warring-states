83302b3739fe7c7217f0e15342d0b487a6a76a9c
GBQT <nheutte@gmail.com>
2024-06-19 23:53:54 +0200

    Added taos and magics to the charsheet

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -6,19 +6,10 @@
 export const preloadHandlebarsTemplates = async function () {
   return loadTemplates([
     // Actor partials.
-    'systems/qin-the-warring-states/templates/actor/parts/actor-features.hbs',
     'systems/qin-the-warring-states/templates/actor/parts/actor-items.hbs',
-    'systems/qin-the-warring-states/templates/actor/parts/actor-spells.hbs',
-    'systems/qin-the-warring-states/templates/actor/parts/actor-effects.hbs',
     'systems/qin-the-warring-states/templates/actor/parts/actor-skills.hbs',
-    'systems/qin-the-warring-states/templates/actor/parts/actor-semblance.hbs',
-    'systems/qin-the-warring-states/templates/actor/parts/actor-aura.hbs',
-    'systems/qin-the-warring-states/templates/actor/parts/actor-dust.hbs',
     "systems/qin-the-warring-states/templates/actor/parts/actor-combat.hbs",
-    // Item partials
-    'systems/qin-the-warring-states/templates/item/parts/item-effects.hbs',
-    //Rolls
-    'systems/qin-the-warring-states/templates/rolls/parts/roll-dialog-content.hbs',
-    'systems/qin-the-warring-states/templates/rolls/parts/attribute-selector.hbs',
+    "systems/qin-the-warring-states/templates/actor/parts/actor-taos.hbs",
+    "systems/qin-the-warring-states/templates/actor/parts/actor-magic.hbs",
   ]);
 };
